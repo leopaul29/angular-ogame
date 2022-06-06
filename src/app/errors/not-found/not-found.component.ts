b1da9925f6c978ef33985b46c94fe24fac898a47
Leo-paul <leopaulmartin@hotmail.fr>
2022-06-06 14:17:37 +0200

    add 404 not found page

---
+++ src/app/errors/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `
+    <h1>404 NOT FOUND</h1>
+  `,
+  styles: [
+  ]
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
